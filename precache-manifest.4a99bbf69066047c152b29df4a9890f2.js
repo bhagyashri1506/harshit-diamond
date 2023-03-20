b3628cb12fbd2771a10a99a1490fc85a533f856d
Harshit <harshitupadhyay872@gmail.com>
2023-03-20 18:29:04 +0530

    Updates

---
+++ precache-manifest.4a99bbf69066047c152b29df4a9890f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c508b5230290746c0f5",
+    "url": "/harshit-diamond/static/css/main.ea358c07.chunk.css"
+  },
+  {
+    "revision": "3c508b5230290746c0f5",
+    "url": "/harshit-diamond/static/js/main.3c508b52.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/harshit-diamond/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "664f64fa86fe17f259f3",
+    "url": "/harshit-diamond/static/js/2.664f64fa.chunk.js"
+  },
+  {
+    "revision": "40875fd60996cbf6d6b5",
+    "url": "/harshit-diamond/static/js/runtime~main.40875fd6.js"
+  },
+  {
+    "revision": "731fc6600ea498dfee168ea53dbb7bfa",
+    "url": "/harshit-diamond/index.html"
+  }
+];
